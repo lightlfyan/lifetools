ebec8a7b52f5d4432e5d34a788d266f58622fe1a
lightlfyan <lightlfyan@gmail.com>
2014-11-05 17:17:26 +0800

    update

--- music/scale_frequency.cs
+++ music/scale_frequency.cs
@@ -8,6 +8,8 @@ public class Music {
     static string[] chars = new string[]{"c", "d", "e", "f", "g", "a", "b"};
     static int[] l = new int[]{1, 2, 2, 1, 2, 2, 2};
 
+    public delegate bool action<T>(T t1, T t2);
+
     private static int inner(int acc, Tuple<string, int> t){
         acc += t.Item2;
         d[t.Item1] = acc;
@@ -20,9 +22,15 @@ public class Music {
     }
 
     static void test(){
+        action<int> a = (i, j) => {
+                            d.Add(chars[j]+i, l[j]);
+                            return true;
+                        };
+                        
         IEnumerable<Tuple<string, int>> v1 = from i in Enumerable.Range(1, 3)
                                           from j in Enumerable.Range(0, 7)
-                                          where fill(chars[j]+i, l[j])
+                                          //where fill(chars[j]+i, l[j])
+                                          where a(i, j)
                                           select Tuple.Create(chars[j]+i, l[j]);
 
         v1.ToList().Aggregate(-1, inner);
